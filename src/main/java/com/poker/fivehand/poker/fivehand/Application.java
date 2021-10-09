27ad8c6cc7c5b8ce7d9f1b32bb6465dd4e8d2f2e
karolis1314 <your_email_address@example.com>
2021-10-09 11:16:45 +0100

    Init commit with the basic springboot dependencies

---
+++ src/main/java/com/poker/fivehand/poker/fivehand/Application.java
@@ -0,0 +1,13 @@
+package com.poker.fivehand.poker.fivehand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
